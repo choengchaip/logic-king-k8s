671b863b12a35780361c193cb6c97c89a8e3f269
Choengchai Phachonyut <singh@finema.co>
2021-07-09 18:54:07 +0700

    Update cluster name at frontend deploy script

--- scripts/logic-frontend-deploy.script.sh
+++ scripts/logic-frontend-deploy.script.sh
@@ -2,5 +2,5 @@
 
 TAG_COMMIT=$1
 
-doctl kubernetes cluster kubeconfig save logicking-k8s
+doctl kubernetes cluster kubeconfig save 9d3698e0-fa57-41a7-9741-0ec5a4e0dff2
 kubectl -n logic set image deployment/logic-frontend logic-frontend="${TAG_COMMIT}"
